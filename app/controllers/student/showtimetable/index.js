4fec326c4c9a8a1656fdaaefb2b99b4465761d28
SurajPalNmims <suraj.pal.EXT@nmims.edu>
2023-08-28 16:08:40 +0530

    changes for aside bar

--- app/controllers/student/showtimetable/index.js
+++ app/controllers/student/showtimetable/index.js
@@ -2,16 +2,16 @@ const student = require('../../../models/Students');
 const studentUtil = require('../../../utils/timetableforstudent');
 
 module.exports = {
- showtimetable: (req,res) =>{   
-  console.log('inside the showtimeatbletabe',)                               
-    Promise.all([student.getSlotForShowTimetable(),student.getDistintRoomList(),student.getTimeslot(),studentUtil.getTimetableforStudent()]).then(result =>{
-      res.render('student/showTimetableStudent.ejs',{
-      minMaxSlotId:JSON.stringify(result[0].recordsets[0]),
-      roomList: JSON.stringify(result[1].recordset),
-      timeSlotList: JSON.stringify(result[2].recordset)
+  showtimetable: (req, res) => {
+    console.log('inside the showtimeatbletabe',)
+    Promise.all([student.getSlotForShowTimetable(), student.getDistintRoomList(), student.getTimeslot()]).then(result => {
+      res.render('admin/students/showTimetableStudent.ejs', {
+        minMaxSlotId: JSON.stringify(result[0].recordsets[0]),
+        roomList: JSON.stringify(result[1].recordset),
+        timeSlotList: JSON.stringify(result[2].recordset)
       })
     })
- }
+  }
 }
 
 
