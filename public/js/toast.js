3ff6f03dd6762c5bbda9ff6e0ceecf6af6132dfd
SurajPalNmims <suraj.pal.EXT@nmims.edu>
2024-04-04 19:38:32 +0530

    change

--- public/js/toast.js
+++ public/js/toast.js
@@ -34,6 +34,40 @@ function createToast(custom) {
   myToast.show();
 }
 
+function createToastRoundSettings(custom) {
+  let {title, msg, type} = {...custom};
+  msg = msg.replaceAll('"','');
+
+  const titleClass = type === 'positive' ? 'text-success' : type === 'negative' ? 'text-danger' : '';
+  const backgroundColor = type === 'positive' ? 'background-success' : type === 'negative' ? 'background-danger' : '';
+
+  const htmlMarkup =
+    `<div aria-atomic="true" aria-live="assertive" class="toast m-3 ${backgroundColor}" 
+      role="alert" id="myAlert"  style="z-index:9999999; position:fixed; top:65px; right:0;">
+        <div class="toast-header ${backgroundColor}">
+          <strong class="me-auto fw-bold fs-5 ${titleClass}">${title} !</strong>
+          <button aria-label="Close" class="btn-close text-dark me-1" data-bs-dismiss="toast" type="button"></button>
+        </div>
+        <div class="toast-body text-dark fw-bold">
+            ${msg}
+        </div>
+    </div>`;
+  
+  function toast() {
+    let template = document.createElement('template');
+    let html = htmlMarkup.trim()
+    template.innerHTML = html
+    return template.content.firstChild
+  }
+  
+  
+  let toastEl = toast();
+  document.body.appendChild(toastEl)
+  const myToast = new Toast(toastEl);
+  myToast._config.delay = 100;
+  myToast.show();
+}
+
 // createToast({
 //   title: "FAILED!",
 //   msg: e.responseJSON.message,
